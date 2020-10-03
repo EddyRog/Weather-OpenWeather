400d71c7b395bbdbd06aa728b25f1d091feb46e0
Eddy <eddytestt@gmail.com>
2020-10-03 12:26:45 +0200

    Initial Commit

---
+++ Weather-OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-OpenWeather
+//
+//  Created by Eddy R on 10/3/20.
+//  Copyright © 2020 EddyR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Weather-OpenWeatherTests/Weather_OpenWeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_OpenWeatherTests.swift
+//  Weather-OpenWeatherTests
+//
+//  Created by Eddy R on 10/3/20.
+//  Copyright © 2020 EddyR. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_OpenWeather
+
+class Weather_OpenWeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
